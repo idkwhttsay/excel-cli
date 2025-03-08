2b72ce485de14145f139e2f598455734c0c793b3
idkwhttsay <danil.tatarinov.00@gmail.com>
2025-03-08 18:57:39 -0500

    init: init nobuild system

---
+++ nob.c
@@ -0,0 +1,26 @@
+// nob.c
+#define NOB_IMPLEMENTATION
+#include "nob.h"
+
+#define CFLAGS "-Wall", "-Wextra", "-std=c11", "-pedantic", "-ggdb"
+
+int main(int argc, char **argv)
+{
+    NOB_GO_REBUILD_URSELF(argc, argv);
+    Nob_Cmd cmd = {0};
+
+    nob_cmd_append(&cmd, "cc", CFLAGS, "-o", "excel-cli", "src/main.c");
+    if (!nob_cmd_run_sync(cmd)) return 1;
+
+    if(argc > 1) {
+        if(strcmp(argv[1], "run") == 0) {
+            Nob_Cmd run = {0};
+            nob_cmd_append(&run, "./excel-cli");
+            if (!nob_cmd_run_sync(run)) return 1;
+        } else {
+            nob_log(NOB_ERROR, "%s is an unknown suncommand", argv[1]);
+        }
+    }
+
+    return 0;
+}

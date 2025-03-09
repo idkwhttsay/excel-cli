e07935f682e0b86c6b349238e649330eb57e6377
idkwhttsay <danil.tatarinov.00@gmail.com>
2025-03-08 19:12:20 -0500

    feat: usage and read get the input file path

--- src/main.c
+++ src/main.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
-    printf("Welcome to Excel-CLI!\n");
+void print_usage(FILE *stream) {
+    fprintf(stream, "Usage: ./excel-cli <input.csv>\n");
+}
+
+int main(int argc, char **argv) {
+    if(argc < 2) {
+        print_usage(stderr);
+        fprintf(stderr, "ERROR: input file is not provided\n");
+        exit(1);
+    }
+
+    const char *input_file_path = argv[1];
+    
+
     return 0;
 }

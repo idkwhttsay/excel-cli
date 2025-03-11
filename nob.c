ceddcec49147188b14b05a8a1ee0ac0d9cdf9a54
idkwhttsay <danil.tatarinov.00@gmail.com>
2025-03-10 21:21:27 -0400

    fix: _WIN_32 to _WIN32

--- nob.c
+++ nob.c
@@ -4,18 +4,35 @@
 
 #define CFLAGS "-Wall", "-Wextra", "-std=c11", "-pedantic", "-ggdb"
 
+#ifdef _WIN32
+#define BINARY_NAME "excel-cli.exe"
+#else
+#define BINARY_NAME "excel-cli"
+#endif
+
+#ifdef _WIN32
+#define EXECUTABLE_NAME "./excel-cli.exe"
+#else
+#define EXECUTABLE_NAME "./excel-cli"
+#endif
+
 int main(int argc, char **argv)
 {
     NOB_GO_REBUILD_URSELF(argc, argv);
     Nob_Cmd cmd = {0};
 
-    nob_cmd_append(&cmd, "cc", CFLAGS, "-o", "excel-cli", "src/main.c");
+#ifdef _WIN32
+    nob_cmd_append(&cmd, "./cl.exe", CFLAGS, "-o", BINARY_NAME, "src/main.c");
+#else
+    nob_cmd_append(&cmd, "cc", CFLAGS, "-o", BINARY_NAME, "src/main.c");
+#endif
+
     if (!nob_cmd_run_sync(cmd)) return 1;
 
     if(argc > 1) {
         if(strcmp(argv[1], "run") == 0) {
             Nob_Cmd run = {0};
-            nob_cmd_append(&run, "./excel-cli", "input.csv");
+            nob_cmd_append(&run, EXECUTABLE_NAME, "input.csv");
             if (!nob_cmd_run_sync(run)) return 1;
         } else if(strcmp(argv[1], "lldb") == 0) {
             Nob_Cmd lldb = {0};
